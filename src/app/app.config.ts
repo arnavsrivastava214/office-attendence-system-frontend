d51dc2b6158e90c6799638ae396f398c9d559247
Arnav Srivastava <arnavsrivastava214@gmail.com>
2025-12-23 15:06:22 +0530

    Enable hash routing for Netlify

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
@@ -8,8 +8,8 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(), 
-    provideAnimations() 
+    provideRouter(routes, withHashLocation()), // 👈 ENABLE HASH STRATEGY
+    provideHttpClient(),
+    provideAnimations()
   ]
 };
